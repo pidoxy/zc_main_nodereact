f57438295aa8fbb732c31a07459c44ee78d92159
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-08-29 00:56:12 +0100

    converted next js frontend aspect to react js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// import Head from 'next/head';
+
+import { PluginContent } from './components/PluginContent';
+import { Sidebar } from './components/Sidebar';
+import { Topbar } from './components/Topbar';
+import styles from './styles/Home.module.css';
+import React, { useState } from 'react';
+
+export const URLContext = React.createContext(null);
+
+export default function App() {
+  const [url, setUrl] = useState("https://api.zuri.chat");
+
+  return (
+    <div className={styles.container}>
+      <URLContext.Provider value={{ setUrl, url }}>
+        {/* <Head>
+          <title>Zuri Chat</title>
+          <link rel="icon" href="/zurichatlogo.svg" />
+        </Head> */}
+        <Sidebar />
+        <div className={styles.room}>
+          <Topbar />
+          <div className={styles.pluginContent}>
+            <PluginContent />
+          </div>
+        </div>
+      </URLContext.Provider>
+    </div>
+  );
+}
+
